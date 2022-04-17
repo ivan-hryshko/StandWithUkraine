452c1672aaa6e2462fde81bd9f91ae91d06344c2
giver0 <giver313@gmail.com>
2022-04-17 17:16:43 +0300

    added css to text

--- StandWithUkraine.js
+++ StandWithUkraine.js
@@ -1,15 +1,16 @@
 const warStartDate = new Date(2022, 1, 24)
 const today = new Date()
-let dayFromStartElem = document.getElementById("dayFromStart")
+let dayFromStartElem = document.getElementById('dayFromStartId')
+let someId = document.getElementById('someId')
 let dayFromStart = today - warStartDate
 console.log(dayFromStart)
-dayFromStart = Math.floor(dayFromStart/(1000*60*60*24))
+dayFromStart = Math.ceil(dayFromStart/(1000*60*60*24))
 
 console.log("Hello world")
 console.log(warStartDate)
 console.log(today)
 console.log(dayFromStart)
 
-dayFromStartElem.innerText(dayFromStart)
+dayFromStartElem.innerText = dayFromStart
 
 
